ac647c1ce868519346776bd056677dcd969bc2db
Izzy <izzysoft@qumran.org>
2003-11-05 18:36:54 +0000

    + added Instance Activity Stats
    git-svn-id: svn+ssh://svn.izzysoft.de/public/osprep/trunk@14 b04bc735-7639-0410-8817-d65b05825077

--- sreport.sh
+++ sreport.sh
@@ -347,6 +347,24 @@ DECLARE
     SELECT sql_text FROM stats\$sqltext WHERE hash_value=hv
      ORDER BY piece;
 
+  CURSOR C_InstAct (db_id IN NUMBER, instnum IN NUMBER, bid IN NUMBER, eid IN NUMBER, ela IN NUMBER, tran IN NUMBER) IS
+    SELECT b.name name,
+           to_char(e.value - b.value,'99,999,999,999,990') total,
+	   to_char(round((e.value - b.value)/ela,2),'99,999,999,990.00') sec,
+	   to_char(round((e.value - b.value)/tran,2),'99,999,999,990.00') txn
+      FROM stats\$sysstat b, stats\$sysstat e
+     WHERE b.snap_id = bid
+       AND e.snap_id = eid
+       AND b.dbid    = db_id
+       AND e.dbid    = db_id
+       AND b.instance_number = instnum
+       AND e.instance_number = instnum
+       AND b.name    = e.name
+       AND e.name NOT IN ( 'logons current','opened cursors current','workarea memory allocated')
+       AND e.value   > b.value
+       AND e.value   > 0
+     ORDER BY b.name;
+
 
 BEGIN
   -- Configuration
@@ -376,7 +394,7 @@ BEGIN
   dbms_output.put_line(L_LINE);
 
   -- Navigation
-  L_LINE := TABLE_OPEN||'<TR><TD><FONT SIZE=-2>[ <A HREF="#snapinfo">SnapShot Info</A> ] '||
+  L_LINE := TABLE_OPEN||'<TR><TD ALIGN="center"><FONT SIZE=-2>[ <A HREF="#snapinfo">SnapShot Info</A> ] '||
             '[ <A HREF="#cachesizes">Cache Sizes</A> ] [ <A HREF="#loads">Load Profile</A> '||
             '] [ <A HREF="#efficiency">Efficiency</A> ]';
   dbms_output.put_line(L_LINE);
@@ -384,7 +402,8 @@ BEGIN
             ' ] [ <A HREF="#waitevents">Wait Events</A> ] [ <A HREF="#bgwaitevents">Background Waits</A> ]';
   dbms_output.put_line(L_LINE);
   L_LINE := ' [ <A HREF="#sqlbygets">SQL by Gets</A> ] [ <A HREF="#sqlbyreads">SQL by Reads</A> ]'||
-	    ' [ <A HREF="#sqlbyexec">SQL by Exec</A> ] [ <A HREF="#sqlbyparse">SQL by Parse</A> ]</TD></TR>';
+	    ' [ <A HREF="#sqlbyexec">SQL by Exec</A> ] [ <A HREF="#sqlbyparse">SQL by Parse</A> ]'||
+	    ' [ <A HREF="#instact">Instance Activity</A> ]</TD></TR>';
   dbms_output.put_line(L_LINE);
   L_LINE := TABLE_CLOSE;
   dbms_output.put_line(L_LINE);
@@ -830,6 +849,22 @@ BEGIN
       dbms_output.put_line(L_LINE);
     END LOOP;
     L_LINE := '</TD></TR>';
+    dbms_output.put_line(L_LINE);
+  END LOOP;
+  L_LINE := TABLE_CLOSE;
+  dbms_output.put_line(L_LINE);
+  dbms_output.put_line('<HR>');
+
+  -- Instance Activity
+  L_LINE := TABLE_OPEN||'<TR><TH COLSPAN="4"><A NAME="#instact">Instance Activity Stats</A></TH></TR>';
+  dbms_output.put_line(L_LINE);
+  L_LINE := ' <TR><TH CLASS="th_sub">Statistic</TH><TH CLASS="th_sub">Total</TH>'||
+	    '<TH CLASS="th_sub">per Second</TH><TH CLASS="th_sub">per TXN</TH></TR>';
+  dbms_output.put_line(L_LINE);
+  FOR R_Inst IN C_InstAct(DBID,INST_NUM,BID,EID,ELA,TRAN) LOOP
+    L_LINE := ' <TR><TD CLASS="td_name">'||R_Inst.name||'</TD><TD ALIGN="right">'||
+              R_Inst.total||'</TD><TD ALIGN="right">'||R_Inst.sec||
+	      '</TD><TD ALIGN="right">'||R_Inst.txn||'</TD></TR>';
     dbms_output.put_line(L_LINE);
   END LOOP;
   L_LINE := TABLE_CLOSE;
