937cc019b8d69c85bb18c2f70f3713ea876cab05
Izzy <izzysoft@qumran.org>
2003-11-06 20:45:48 +0000

    + added Undo Seg Stats
    git-svn-id: svn+ssh://svn.izzysoft.de/public/osprep/trunk@27 b04bc735-7639-0410-8817-d65b05825077

--- sreport.sh
+++ sreport.sh
@@ -809,6 +809,27 @@ DECLARE
        AND begin_time < to_date(etime, 'DD.MM.YYYY HH24:MI:SS')
      GROUP BY undotsn;
 
+  CURSOR C_UST (db_id IN NUMBER, instnum IN NUMBER, bid IN NUMBER, eid IN NUMBER, btime IN VARCHAR2, etime IN VARCHAR2) IS
+    SELECT undotsn, endt,undob,txcnt,maxq,maxc,snol,nosp,blkst
+    FROM ( SELECT undotsn,
+                to_char(end_time,'DD.MM.YYYY HH24:MI') endt,
+                to_char(undoblks,'99,999,999') undob,
+                to_char(txncount,'99,999,999') txcnt,
+	        to_char(maxquerylen,'999,999,999') maxq,
+	        to_char(maxconcurrency,'999,999') maxc,
+	        to_char(ssolderrcnt,'99,999') snol,
+	        to_char(nospaceerrcnt,'99,999') nosp,
+	        unxpstealcnt||'/'||unxpblkrelcnt||'/'||
+	        unxpblkreucnt||' / '||expstealcnt||'/'||
+	        expblkrelcnt||'/'||expblkreucnt blkst
+           FROM stats\$undostat
+          WHERE dbid = db_id
+            AND instance_number = instnum
+            AND end_time > to_date(btime, 'DD.MM.YYYY HH24:MI:SS')
+            AND begin_time < to_date(etime, 'DD.MM.YYYY HH24:MI:SS')
+       ORDER BY begin_time desc )
+   WHERE rownum < 25;
+
 
 BEGIN
   -- Configuration
@@ -1566,7 +1587,7 @@ BEGIN
   dbms_output.put_line(L_LINE);
   dbms_output.put_line('<HR>');
 
-  -- Undo Segs
+  -- Undo Segs Summary
   L_LINE := TABLE_OPEN||'<TR><TH COLSPAN="8"><A NAME="#undo">Undo Segment Summary</A></TH></TR>'||
             ' <TR><TD COLSPAN="8" ALIGN="center">Undo Segment block stats<BR>'||
 	    'uS - unexpired Stolen, uR - unexpired Released, uU - unexpired reUsed<BR>';
@@ -1582,6 +1603,30 @@ BEGIN
   dbms_output.put_line(L_LINE);
   FOR R_USS IN C_USS(DBID,INST_NUM,BID,EID,BTIME,ETIME) LOOP
     L_LINE := ' <TR><TD CLASS="td_name" ALIGN="right">'||R_USS.undotsn||'</TD><TD ALIGN="right">'||
+              R_USS.undob||'</TD><TD ALIGN="right">'||R_USS.txcnt||
+	      '</TD><TD ALIGN="right">'||R_USS.maxq||'</TD>';
+    dbms_output.put_line(L_LINE);
+    L_LINE := '<TD ALIGN="right">'||R_USS.maxc||'</TD><TD ALIGN="right">'||
+              R_USS.snol||'</TD><TD ALIGN="right">'||R_USS.nosp||'</TD><TD ALIGN="right">'||
+	      R_USS.blkst||'</TD></TR>';
+    dbms_output.put_line(L_LINE);
+  END LOOP;
+  L_LINE := TABLE_CLOSE;
+  dbms_output.put_line(L_LINE);
+
+  -- Undo Segs Stat
+  L_LINE := TABLE_OPEN||'<TR><TH COLSPAN="8"><A NAME="#undo">Undo Segment Statistics</A></TH></TR>'||
+            ' <TR><TD COLSPAN="8" ALIGN="center">Ordered by Time desc</TD></TR>';
+  dbms_output.put_line(L_LINE);
+  L_LINE := ' <TR><TH CLASS="th_sub">End Time</TH><TH CLASS="th_sub">Undo Blocks</TH>'||
+            '<TH CLASS="th_sub"># TXN</TH><TH CLASS="th_sub">Max Qry Len (s)</TH>'||
+	    '<TH CLASS="th_sub">Max Tx Concurcy</TH>';
+  dbms_output.put_line(L_LINE);
+  L_LINE := '<TH CLASS="th_sub">Snapshot Too Old</TH><TH CLASS="th_sub">'||
+            'Out of Space</TH><TH CLASS="th_sub">uS/ur/uU / eS/eR/eU</TH></TR>';
+  dbms_output.put_line(L_LINE);
+  FOR R_USS IN C_UST(DBID,INST_NUM,BID,EID,BTIME,ETIME) LOOP
+    L_LINE := ' <TR><TD CLASS="td_name" ALIGN="right">'||R_USS.endt||'</TD><TD ALIGN="right">'||
               R_USS.undob||'</TD><TD ALIGN="right">'||R_USS.txcnt||
 	      '</TD><TD ALIGN="right">'||R_USS.maxq||'</TD>';
     dbms_output.put_line(L_LINE);
