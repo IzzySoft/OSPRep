d7aae0801db24a70238074249e1e1705d0970c8c
Izzy <izzysoft@qumran.org>
2003-11-06 20:44:29 +0000

    + added RBS Stats
    git-svn-id: svn+ssh://svn.izzysoft.de/public/osprep/trunk@24 b04bc735-7639-0410-8817-d65b05825077

--- sreport.sh
+++ sreport.sh
@@ -750,6 +750,28 @@ DECLARE
        AND e.total_wait# - nvl(b.total_wait#,0) > 0
      ORDER BY waits desc, reqs desc;
 
+  CURSOR C_RBS (db_id IN NUMBER, instnum IN NUMBER, bid IN NUMBER, eid IN NUMBER) IS
+    SELECT b.usn rbs#,
+           to_char(e.gets - b.gets,'999,990.0') gets,
+	   to_char(to_number(decode(e.gets,b.gets,NULL,
+	          (e.waits - b.waits) * 100 / (e.gets - b.gets) )),
+		  '990.00') waits,
+	   to_char(e.writes - b.writes,'999,999,999,990') writes,
+	   to_char(e.wraps - b.wraps,'999,999') wraps,
+	   to_char(e.shrinks - b.shrinks,'999,999') shrinks,
+	   to_char(e.extends - b.extends,'999,999') extends
+      FROM stats\$rollstat b, stats\$rollstat e
+     WHERE b.snap_id = bid
+       AND e.snap_id = eid
+       AND b.dbid    = db_id
+       AND e.dbid    = db_id
+       AND b.dbid    = e.dbid
+       AND b.instance_number = instnum
+       AND e.instance_number = instnum
+       AND b.instance_number = e.instance_number
+       AND e.usn     = b.usn
+     ORDER BY e.usn;
+
 
 BEGIN
   -- Configuration
@@ -794,7 +816,8 @@ BEGIN
             ' [ <A HREF="#bufpool">Buffer Pool</A> ] [ <A HREF="#recover">Instance Recovery</A> ]'||
 	    ' [ <A HREF="#bufwait">Buffer Waits</A> ]';
   dbms_output.put_line(L_LINE);
-  L_LINE := ' [ <A HREF="#pga">Memory Stats</A> ] [ <A HREF="#enq">Enqueue Activity</A> ]</TD></TR>';
+  L_LINE := ' [ <A HREF="#pga">Memory Stats</A> ] [ <A HREF="#enq">Enqueue Activity</A> ]'||
+            ' [ <A HREF="#rbs">RBS</A> ]</TD></TR>';
   dbms_output.put_line(L_LINE);
   L_LINE := TABLE_CLOSE;
   dbms_output.put_line(L_LINE);
@@ -1450,6 +1473,34 @@ BEGIN
     dbms_output.put_line(L_LINE);
     L_LINE := '<TD ALIGN="right">'||R_Enq.waits||'</TD><TD ALIGN="right">'||
               R_Enq.awttm||'</TD><TD ALIGN="right">'||R_Enq.wttm||'</TD></TR>';
+    dbms_output.put_line(L_LINE);
+  END LOOP;
+  L_LINE := TABLE_CLOSE;
+  dbms_output.put_line(L_LINE);
+  dbms_output.put_line('<HR>');
+
+  -- RBS Stats
+  L_LINE := TABLE_OPEN||'<TR><TH COLSPAN="7"><A NAME="#rbs">Rollback Segments Stats</A></TH></TR>'||
+            ' <TR><TD COLSPAN="7" ALIGN="center">A high value for "Pct Waits" '||
+	    'suggests more rollback segments may be required<BR>RBS stats may ';
+  dbms_output.put_line(L_LINE);
+  L_LINE := 'not be accurate between begin and end snaps when using Auto Undo '||
+            'Management, as RBS may be dynamically created and dropped as needed</TD></TR>';
+  dbms_output.put_line(L_LINE);
+  L_LINE := ' <TR><TH CLASS="th_sub">RBS#</TH><TH CLASS="th_sub">Trans Table Gets</TH>'||
+            '<TH CLASS="th_sub">Pct Waits</TH><TH CLASS="th_sub">Undo Bytes Written</TH>'||
+	    '<TH CLASS="th_sub">Wraps</TH>';
+  dbms_output.put_line(L_LINE);
+  L_LINE := '<TH CLASS="th_sub">Shrinks</TH><TH CLASS="th_sub">'||
+            'Extends</TH></TR>';
+  dbms_output.put_line(L_LINE);
+  FOR R_RBS IN C_RBS(DBID,INST_NUM,BID,EID) LOOP
+    L_LINE := ' <TR><TD CLASS="td_name">'||R_RBS.rbs#||'</TD><TD ALIGN="right">'||
+              R_RBS.gets||'</TD><TD ALIGN="right">'||R_RBS.waits||
+	      '</TD><TD ALIGN="right">'||R_RBS.writes||'</TD>';
+    dbms_output.put_line(L_LINE);
+    L_LINE := '<TD ALIGN="right">'||R_RBS.wraps||'</TD><TD ALIGN="right">'||
+              R_RBS.shrinks||'</TD><TD ALIGN="right">'||R_RBS.extends||'</TD></TR>';
     dbms_output.put_line(L_LINE);
   END LOOP;
   L_LINE := TABLE_CLOSE;
