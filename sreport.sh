749b8f2bed37a892b854a19c73bdfdcdb1411545
Izzy <izzysoft@qumran.org>
2003-11-10 19:16:21 +0000

    + added SGA BreakDown Diff
    git-svn-id: svn+ssh://svn.izzysoft.de/public/osprep/trunk@35 b04bc735-7639-0410-8817-d65b05825077

--- sreport.sh
+++ sreport.sh
@@ -963,6 +963,25 @@ DECLARE
        AND instance_number = instnum
      ORDER BY name;
 
+  CURSOR C_SGABreak (db_id IN NUMBER, instnum IN NUMBER, bid IN NUMBER, eid IN NUMBER) IS
+    SELECT nvl(replace(b.pool,'pool',''),'&nbsp;') pool,
+           b.name name,
+	   to_char(b.bytes,'999,999,999,999') snap1,
+	   to_char(e.bytes,'999,999,999,999') snap2,
+	   to_char(100* (e.bytes - b.bytes)/b.bytes,'990.00') diff
+      FROM stats\$sgastat b, stats\$sgastat e
+     WHERE b.snap_id = bid
+       AND e.snap_id = eid
+       AND b.dbid    = db_id
+       AND e.dbid    = db_id
+       AND b.dbid    = e.dbid
+       AND b.instance_number = instnum
+       AND e.instance_number = instnum
+       AND b.instance_number = e.instance_number
+       AND b.name    = e.name
+       AND nvl(b.pool,'a')   = nvl(e.pool,'a')
+     ORDER BY b.pool, b.name;
+
 
 BEGIN
   -- Configuration
@@ -1898,7 +1917,9 @@ BEGIN
 
   -- SGA Memory Summary
   L_LINE := TABLE_OPEN||'<TR><TH COLSPAN="2"><A NAME="#sga">SGA Memory Summary</A></TH></TR>'||
-            ' <TR><TH CLASS="th_sub">SGA Region</TH><TH CLASS="th_sub">Size in Bytes</TH>';
+            ' <TR><TD COLSPAN="2" ALIGN="center">Values at the time of the End SnapShot</TD></TR>';
+  dbms_output.put_line(L_LINE);
+  L_LINE := ' <TR><TH CLASS="th_sub">SGA Region</TH><TH CLASS="th_sub">Size in Bytes</TH>';
   dbms_output.put_line(L_LINE);
   I1 := 0;
   FOR R_SGASum in C_SGASum(DBID,INST_NUM,BID,EID) LOOP
@@ -1909,6 +1930,23 @@ BEGIN
   END LOOP;
   L_LINE := ' <TR><TD>Sum</TD><TD ALIGN="right">'||to_char(I1,'999,999,999,990')||
             '</TD></TR>'||TABLE_CLOSE;
+  dbms_output.put_line(L_LINE);
+
+  -- SGA breakdown diff
+  L_LINE := TABLE_OPEN||'<TR><TH COLSPAN="5"><A NAME="#sga">SGA BreakDown Difference</A></TH></TR>'||
+            ' <TR><TH CLASS="th_sub">Pool</TH><TH CLASS="th_sub">Name</TH>'||
+	    '<TH CLASS="th_sub">Begin Value</TH>';
+  dbms_output.put_line(L_LINE);
+  L_LINE := '<TH CLASS="th_sub">End Value</TH><TH CLASS="th_sub">% Diff</TH></TR>';
+  dbms_output.put_line(L_LINE);
+  FOR R_SGASum in C_SGABreak(DBID,INST_NUM,BID,EID) LOOP
+    L_LINE := ' <TR><TD CLASS="td_name">'||R_SGASum.pool||'</TD><TD CLASS="td_name">'||
+              R_SGASum.name||'</TD><TD ALIGN="right">'||R_SGASum.snap1||
+	      '</TD><TD ALIGN="right">'||R_SGASum.snap2||'</TD><TD ALIGN="right">'||
+	      R_SGASum.diff||'</TD></TR>';
+    dbms_output.put_line(L_LINE);
+  END LOOP;
+  L_LINE := TABLE_CLOSE;
   dbms_output.put_line(L_LINE);
   dbms_output.put_line('<HR>');
 
