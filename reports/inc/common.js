3c0b7b36139daa6550522b54e0901385b05485fd
Izzy <izzysoft@qumran.org>
2009-02-03 11:31:52 +0000

    * center the graph (*_chart.html)
    git-svn-id: svn+ssh://svn.izzysoft.de/public/osprep/trunk@413 b04bc735-7639-0410-8817-d65b05825077

--- reports/inc/common.js
+++ reports/inc/common.js
@@ -5,14 +5,15 @@ if (max == 0) max = 1;
 
 // Create Diagram
 function mkdiag() {
-  D.SetFrame(80, 120, 540, 380);
- // ScreenLeftX, TopY, RightX, BottomY
-  D.SetBorder(bid, eid, 0, max + max/20);
- // DiagLeftX, RightX, BottomY, TopY
-  D.SetText("", "", "<B>"+parent.dname+"</B>");
- // ScaleX, ScaleY, Title
-  D.Draw("#DDDDDD", "#000000", false, "");
- // DrawColor, TextColor, isScaleText [, ToolTip [, Action]]
+  // Center graph
+  wwid = document.body.offsetWidth - 460; // Screen width w/o graph
+  x1 = wwid/2;   // LeftX for graph
+  x2 = x1 + 460; // RightX for graph
+  D.SetFrame(x1, 140, x2, 400); // ScreenLeftX, TopY, RightX, BottomY
+  // Set properties
+  D.SetBorder(bid, eid, 0, max + max/20); // DiagLeftX, RightX, BottomY, TopY
+  D.SetText("", "", "<B>"+parent.dname+"</B>"); // ScaleX, ScaleY, Title
+  D.Draw("#DDDDDD", "#000000", false, ""); // DrawColor, TextColor, isScaleText [, ToolTip [, Action]]
 }
 
 // Draw diagram for specified stat
